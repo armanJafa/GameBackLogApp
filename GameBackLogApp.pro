ae9d4f0ebad438cb072db63441708339f43af72f
ArmanJafa <armanj6@gmail.com>
2017-06-15 16:08:24 -0700

    closes #1

--- GameBackLogApp.pro
+++ GameBackLogApp.pro
@@ -25,10 +25,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp
+        mainwindow.cpp \
+    videogames.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+    videogames.h
 
 FORMS += \
         mainwindow.ui
---
+++ videogames.cpp
@@ -0,0 +1,71 @@
+#include "videogames.h"
+
+VideoGames::VideoGames()
+{
+    title = "";
+    platform = "";
+    developer = "";
+    ESRB = "";
+    publisher = "";
+    yearOfRelease = 0;
+}
+
+QString VideoGames::GetTitle()
+{
+    return title;
+}
+
+QString VideoGames::GetPlatform()
+{
+    return platform;
+}
+
+QString VideoGames::GetDeveloper()
+{
+    return developer;
+}
+
+QString VideoGames::GetESRB()
+{
+    return ESRB;
+}
+
+QString VideoGames::GetPublisher()
+{
+    return publisher;
+}
+
+int VideoGames::GetYearOfRelease()
+{
+    return YearOfRelease();
+}
+
+void VideoGames::SetTitle(QString newTitle)
+{
+    title = newTitle;
+}
+
+void VideoGames::SetPlatform(QString newPlatform)
+{
+    platform = newPlatform;
+}
+
+void VideoGames::SetDeveloper(QString newDeveloper)
+{
+    developer = newDeveloper;
+}
+
+void VideoGames::SetESRB(QString newESRB)
+{
+    ESRB = newESRB;
+}
+
+void VideoGames::SetPublisher(QString newPublisher)
+{
+    publisher = newPublisher;
+}
+
+void VideoGames::SetYearOfRelease(QString newYearOfRelease)
+{
+    yearOfRelease = newYearOfRelease;
+}
